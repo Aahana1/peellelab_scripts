e966a6e2854164bf3ee52d6f4debf02c3f17e640
Mike Jones <jones.mike@wustl.edu>
2019-12-03 15:36:07 -0600

    improvements to the crawlers

--- aa_utils/aa_jpeg_crawler_standalone.m
+++ aa_utils/aa_jpeg_crawler_standalone.m
@@ -1,7 +1,11 @@
-% function aa_jpeg_crawler(aap, exclude_list)
-function aa_jpeg_crawler(aap, path_regex)
+function aa_jpeg_crawler_standalone(aap, path_regex)
+
 %
 % created an indexed report
+%
+% this version copies the jpegs in the results tree
+% a local folder and uses relative pathnames into it
+% (so the folder can be zipped and shared and not break the links)
 %
 %	usage: aa_jpeg_crawler(aap, path_regex)
 %
@@ -9,10 +13,21 @@ function aa_jpeg_crawler(aap, path_regex)
 %
 %	aap - aap struct of analysis to crawl
 %
+% OPTIONAL
+%
 %   path_regex - only include files in the report having this regex
 %   in their pathname (e.g. "firstlevel_model" will only include files
 %   ./*firstlevel_model*/../*.jpg in the report). NB: uses the
 %    pathname not the filename.     
+%
+% FINISH ME
+%
+%   1) need to add diagnostic*.txt files as plantext (current jpeg conversion
+%   gives wonky looking results). Also: need to start adding a
+%   "diagnostic_" prefix to diagnostic txt files (can't search on .txt bc
+%   that includes all of the MD5 stream identifiers)
+%
+%   2) look into using ln rather than cp to save disk space.
 %
 % Usage
 %
@@ -31,17 +46,20 @@ function aa_jpeg_crawler(aap, path_regex)
 %
 % CHANGE HISTORY
 %
-% 12/2019 - change exclude list to include (path_regex); add "no images found"
+% 11/2019 - include both .jpg and .jpeg files; add "no images found"
 %           to empty section headers
-% 11/2019 - include both .jpg and .jpeg files
+% 10/2019 - order section files by type rather than by subject
+% 10/2019 - remove exclude option to simplify (wasn't really useful)
+% 10/2019 - standalone version using cpio
 % 03/2019 - add exclusion list
 % 07/2018 - make sections collapsible
 %
 
-savedir = pwd;
 
-if (isempty(aap))
-	disp('Usage: aa_jpeg_crawler(aap,[path_regex])');
+% sanity checks
+
+if (nargin < 1 || ~isstruct(aap) || isempty(aap))
+	disp('Usage: aa_jpeg_crawler(aap[, path_regex])');
 	return;
 end
 
@@ -49,10 +67,13 @@ if (nargin < 2)
     path_regex = '';
 end
 
+savedir = pwd;
+
 cd([aap.acq_details.root '/' aap.directory_conventions.analysisid]);
 
 % first thing, see if provenance exists and needs converted
-% UPDATE: best to delete the old jpg so we force it to be current
+
+% best to delete the old jpg so we force it to be current
 
 system('rm -f aap_prov.jpg');
 
@@ -87,23 +108,45 @@ for index = 1:numel(root_diagnostic_files)
 end
 
 
-% make a list of all the jpg/jpegs under the directory
-%
-% sorting on field #9 works well, i.e.
-%
-% status = system('find -s `pwd` -name \*.jpg | sort -t/ -k9 > montage_temp.txt');
-%
-% (this is actually root depth + 4)
+% make a copy of all the jpegs in the report" directory
 
-root_depth = numel(split(aap.acq_details.root,'/'));
-    
-command = sprintf('find -s -E `pwd` -type f  -path ''*%s*''  -iregex ''.*[Jj][Pp][Ee]?[Gg]'' | sort -t/ -k%d > montage_temp.txt', path_regex, root_depth+4);
+system('rm -rf aa_report'); % delete old contents if exist
+mkdir('aa_report');
+
+% command = sprintf('find . -path ''./*%s*'' -name ''*.jpg'' | cpio -pdm aa_report', path_regex);
+% command = sprintf('find -s -E `pwd` -type f -path ''*%s*''  -iregex ''.*[Jj][Pp][Ee]?[Gg]'' | cpio -pdm aa_report', path_regex);
+command = sprintf('find -s -E . -type f -path ''*%s*''  -iregex ''.*[Jj][Pp][Ee]?[Gg]'' | cpio -pdm aa_report', path_regex);
+
+
+% find . -name '*.jpg'
+% find -s -E `pwd` -type f -iregex '.*[Jj][Pp][Ee]?[Gg]'
 
 status = system(command);
 
 if (status); cleanup_and_exit(1); end
 
-fid = fopen('aa_jpeg_crawler.htm','w');
+%  the rest of report generation should work as usual -- we just need to cd
+%  into the report directory and use relative pathnames in the html -- see
+%  change in html_add_image
+
+cd('aa_report');
+
+% make a list of all the jpegs under the directory
+%
+% sorting on field #9 works well, i.e.
+%
+% status = system('find -s `pwd` -name \*.jpg | sort -t/ -k9 > montage_temp.txt');
+%
+% this is actually root depth + 4:
+
+root_depth = numel(split(aap.acq_details.root,'/'));
+% command = sprintf('find -s `pwd` -name \\*.jpg | sort -t/ -k%d > montage_temp.txt', root_depth+4);
+command = sprintf('find -s -E `pwd` -type f  -path ''*%s*''  -iregex ''.*[Jj][Pp][Ee]?[Gg]'' | sort -t/ -k%d > montage_temp.txt', path_regex, root_depth+4);
+status = system(command);
+
+if (status); cleanup_and_exit(1); end
+
+fid = fopen('aa_report.htm','w');
 if (fid < 0); cleanup_and_exit(2); end
 
 html_make_head(fid);
@@ -116,19 +159,19 @@ html_close(fid);
 cleanup_and_exit(0);
 cd(savedir);
 
+
 %-----------------------------------------------------------------------------------------------------------------------------------
 % cleanup_and_exit - THIS MUST BE NESTED FOR VARIABLE ACCESS
 %-----------------------------------------------------------------------------------------------------------------------------------
 
 function cleanup_and_exit(ierr)
 
-	system('rm -f montage_temp01.txt');
 	system('rm -f montage_temp.txt');
 	if exist('fid');fclose(fid);end
 	cd(savedir);
 	if (ierr)
 		if exist('aap','var')
-			aas_log(aap, true, sprintf('\n%s: Montage generation failed (ierr = %d.\n', mfilename, ierr));
+			aas_log(aap, true, sprintf('\n%s: Montage generation failed (ierr = %d).\n', mfilename, ierr));
 		else
 			error('Montage generation failed (ierr = %d).\n', ierr);
 		end
@@ -210,7 +253,7 @@ function html_make_head(fid)
     fprintf(fid,'%s\n','    overflow: hidden;');
     fprintf(fid,'%s\n','    background-color: #f1f1f1;');
     fprintf(fid,'%s\n','}');
-	
+
 	fprintf(fid,'%s\n','</style>');
 	fprintf(fid,'%s\n','</head>');
 	fprintf(fid,'%s\n','<body>');
@@ -290,7 +333,12 @@ end
 
 function html_open_div(fid)
 
-	% need an extra div tag here to balance the first call to
+	% the margin-left percent here needs to play nice with the toc css
+	% also note: double %% is % literal
+
+% 	fprintf(fid, '<div style="margin-left:12%%;padding:1px 16px;height:1000px;">\n');
+
+	% new -- need an extra div tag here to balance the first call to
 	% collapsible button...
 	
 	fprintf(fid, '<div style="margin-left:12%%;padding:1px 16px;height:1000px;"><div>\n');
@@ -328,8 +376,6 @@ function html_add_jpegs(fid, aap)
 
 	for index = 1:numel(tasknames)
 		
-		current_image_type = [];
-
 		% handle multiple module appearance
 		
 		module_name = tasknames{index};
@@ -344,8 +390,8 @@ function html_add_jpegs(fid, aap)
 			% grep returns status 1 if nothing found (but its not an error)
 
 			if (status>1); cleanup_and_exit(3); end
-            
-            if (isempty(jpeg_list))
+
+           if (isempty(jpeg_list))
                 fprintf(fid,'<h2 id="tag%02d">%s (no images found)</h2>\n', tagnum, stagename);
             else
                 fprintf(fid,'<h2 id="tag%02d">%s</h2>\n', tagnum, stagename);
@@ -355,13 +401,88 @@ function html_add_jpegs(fid, aap)
 
 			%  split adds a (wrong) entry if jpeg_list is empty...
 
-			if (~isempty(jpeg_list));jpeg_list = split(deblank(jpeg_list));end
+			if (~isempty(jpeg_list));jpeg_list = split(deblank(jpeg_list));end			
 
-			% jpeg_list should be sorted by subject. We put all images of a type in a
-			% row and insert a linebreak each time we encounter a new type
-			% TEST ME -- this may not be needed with tasklist-based sorting
-			
-			for jindex = 1:numel(jpeg_list)
+            % try ordering the jpegs according to type rather than SID
+            % That is, rather than adding the jpegs in this order:
+            %
+            %   /path/to/aamod_wawa/sub-01/foo.jpg
+            %   /path/to/aamod_wawa/sub-01/bar.jpg
+            %   /path/to/aamod_wawa/sub-02/foo.jpg
+            %   /path/to/aamod_wawa/sub-02/bar.jpg
+            %
+            % add them in this order
+            %
+            %   /path/to/aamod_wawa/sub-01/foo.jpg
+            %   /path/to/aamod_wawa/sub-02/foo.jpg
+            %   /path/to/aamod_wawa/sub-01/bar.jpg
+            %   /path/to/aamod_wawa/sub-02/bar.jpg
+            %
+            % problem: the jpgs can be named anything --
+            % sometimes the SID is part of the path and sometimes
+            % part of the filename and sometimes both (actually
+            % we can't even be sure of the order of the files
+            % returned by grep for the module).
+            %
+            % Algorithm: 
+            %
+            % grep on the first SID-1 (aap.acq_details.subjects(1).subjname)
+            % to generate a filetype list (we assume all subjects have
+            % the same files). Then, for the first entry in the filetype list, 
+            % loop over all subjects, generate a grep string by replacing 
+            % SID-1 with SID-k and add the match to the HTML. Repeat for 
+            % all entries in the filetype list
+            %
+            
+            SID0 = aap.acq_details.subjects(1).subjname;
+            
+            filetype_list = [];
+            
+  			[ status, filetype_list ] = system(sprintf('grep -e %s montage_temp.txt | grep -e %s', stagename, SID0));
+
+			% grep returns status 1 if nothing found (but its not an error)
+
+			if (status>1); cleanup_and_exit(3); end
+			if (~isempty(filetype_list)); filetype_list = split(deblank(filetype_list)); end
+            
+            checklist = [];
+            
+            for tindex = 1:numel(filetype_list)
+                
+   				[ ~,image_type,~ ] = fileparts(filetype_list{tindex});     
+                section_header = erase(image_type, SID0);
+                section_header = erase(section_header, '_C_');
+                html_add_linebreak(fid, section_header);
+                
+                for sindex = 1:numel(aap.acq_details.subjects)
+                    
+                    SID = aap.acq_details.subjects(sindex).subjname;
+                    regex = strrep(filetype_list{tindex},SID0,SID);
+                    
+                    for jindex = 1:numel(jpeg_list)
+
+                        jpeg_name = jpeg_list{jindex};
+    
+                        if (strcmp(jpeg_name,regex))
+                            html_add_image(fid, jpeg_name);            
+                            checklist = [ checklist jindex ];
+                            break;
+                        end
+                        
+                    end
+
+                end
+                
+            end
+
+            % some modules don't have SID-based jpgs (cf. secondlevel)
+            % plot any stragglers using vanilla sort order       
+            
+            current_image_type = [];
+            
+            stragglers = setdiff(1:numel(jpeg_list),checklist);
+            
+            for jindex = stragglers
 
 				jpeg_name = jpeg_list{jindex};
 
@@ -378,13 +499,13 @@ function html_add_jpegs(fid, aap)
 
 				html_add_image(fid, jpeg_name);
 
-			end
-			
+            end                        
+                        
 			fprintf('.');
 
 		end
 
-	end
+    end
 	
 	fprintf('\n');
 	
@@ -403,11 +524,15 @@ function html_add_image(fid, image_path)
 	
 	if strfind(image_path,'/.')
 		return;
-	end
+    end
+    
+    % new - change to relative pathing
 
+    image_path = strrep(image_path,pwd,'.');
+    
 	h = imfinfo(image_path);
 
-	% REVISIT: scale factor by numel(jpeg_list)?
+	% REVISIT: maybe modify scale factor by numel(jpeg_list)?
 
 	scaling_factor = 500;
 
@@ -419,7 +544,8 @@ function html_add_image(fid, image_path)
 		width = round(h.Width * scaling_factor / h.Height);
 	end
 
-	fprintf(fid, sprintf('<a href="file://%s" target="_blank">\n', image_path));
+% 	fprintf(fid, sprintf('<a href="file://%s" target="_blank">\n', image_path));
+	fprintf(fid, sprintf('<a href="%s" target="_blank">\n', image_path));       % relative pathing 
 	fprintf(fid, sprintf('<img src="%s" title="%s" height="%d" width="%d"></a>\n', image_path, image_path, height, width));
 
 end
@@ -434,9 +560,13 @@ function html_add_linebreak(fid,s)
 	if (isempty(s))
 		fprintf(fid, '<br/>\n');
 	else
+% 		fprintf(fid, '<h4>%s</h4>\n', s);
+% 		fprintf(fid, '<button class="collapsible">%s</button>\n', s);
 		fprintf(fid, '</div><br/><br/><button class="collapsible">%s</button>\n', s);
 		fprintf(fid, '<div class="content"><br/>\n');
-    end
+	end
+
+	
 	
 end
 
@@ -445,6 +575,7 @@ end
 %-----------------------------------------------------------------------------------------------------------------------------------
 
 function html_close_div(fid)
+% 	fprintf(fid, '</div>');
 	fprintf(fid, '</div><br/><br/>');
 end
 
@@ -454,26 +585,24 @@ end
 
 function html_close(fid)
 
+    fprintf(fid,'%s\n','<script>');
+    fprintf(fid,'%s\n','var coll = document.getElementsByClassName("collapsible");');
+    fprintf(fid,'%s\n','var i;');
+    fprintf(fid,'%s\n','');
+    fprintf(fid,'%s\n','for (i = 0; i < coll.length; i++) {');
+    fprintf(fid,'%s\n','    coll[i].addEventListener("click", function() {');
+    fprintf(fid,'%s\n','        this.classList.toggle("active");');
+    fprintf(fid,'%s\n','        var content = this.nextElementSibling;');
+    fprintf(fid,'%s\n','        if (content.style.display === "none") {');
+    fprintf(fid,'%s\n','            content.style.display = "block";');
+    fprintf(fid,'%s\n','        } else {');
+    fprintf(fid,'%s\n','            content.style.display = "none";');
+    fprintf(fid,'%s\n','        }');
+    fprintf(fid,'%s\n','    });');
+    fprintf(fid,'%s\n','}');
+    fprintf(fid,'%s\n','</script>');
 
-fprintf(fid,'%s\n','<script>');
-fprintf(fid,'%s\n','var coll = document.getElementsByClassName("collapsible");');
-fprintf(fid,'%s\n','var i;');
-fprintf(fid,'%s\n','');
-fprintf(fid,'%s\n','for (i = 0; i < coll.length; i++) {');
-fprintf(fid,'%s\n','    coll[i].addEventListener("click", function() {');
-fprintf(fid,'%s\n','        this.classList.toggle("active");');
-fprintf(fid,'%s\n','        var content = this.nextElementSibling;');
-fprintf(fid,'%s\n','        if (content.style.display === "none") {');
-fprintf(fid,'%s\n','            content.style.display = "block";');
-fprintf(fid,'%s\n','        } else {');
-fprintf(fid,'%s\n','            content.style.display = "none";');
-fprintf(fid,'%s\n','        }');
-fprintf(fid,'%s\n','    });');
-fprintf(fid,'%s\n','}');
-fprintf(fid,'%s\n','</script>');
-
-fprintf(fid, '</body></html>');
-
+	fprintf(fid, '</body></html>');
 end
 
 

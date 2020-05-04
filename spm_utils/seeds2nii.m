4810d5bb0771761535be116624edaf6387a29aef
Mike Jones <jones.mike@wustl.edu>
2020-05-04 12:41:07 -0500

    add atlas mode to seeds2nii

--- spm_utils/seeds2nii.m
+++ spm_utils/seeds2nii.m
@@ -5,15 +5,23 @@ function [ errflag,errstring ] = seeds2nii(centers, radii, template_fname, nii_f
 %
 % INPUTS
 %
-%   centers         - 3xn XYZ defining seed centers
+%   centers         - 3xn XYZ defining seed centers. (NB: 3xn -- i.e. COLUMN WISE)
 %   radii           - nx1 seed radii, or a single value that will be applied to all
 %   template_fname  - template image to use (fullpath if not in working dir)
-%                     Ex: '/Applications/MATLAB_R2016b.app/toolbox/spm12/toolbox/OldNorm/T1.nii';
+%                     Ex: '/Applications/spm12/toolbox/OldNorm/T1.nii';
 %   nii_fname       - fname of nii to create (saved in pwd if not fullpath)
+%                     (NB: if the file exists it will be overwritten)
 %
 %   OPTIONAL
 %
-%   voxel_value - voxel value to set inside ROI (default = 1)
+%   voxel_value - voxel value for ROI (default = 1)
+%
+% ATLAS MODE
+%
+% if voxel_value = "index", then the voxel value for each ROI is set equal
+% to the ordinal position of the seed in "centers" (i.e., all voxels in the 
+% first ROI have a value of 1, all in the second have a value of 2, and so
+% on. This is useful for generating an indexed ROI atlas.
 %
 % NOTES
 %
@@ -21,6 +29,11 @@ function [ errflag,errstring ] = seeds2nii(centers, radii, template_fname, nii_f
 % 2) uses marsbar for voxel extraction
 % 3) A voxel value > 1 is recommended when overlaying ROI on tmaps
 %    - see comments in overlay_nifti.m
+%
+% CHANGE HISTORY
+%
+% 05/2020 [MSJ] - added "atlas mode" option
+% 2019 [MSJ] - new
 %
 
 errflag = 0;
@@ -46,18 +59,20 @@ if (isempty(p))
     return;
 end
 
-% in case caller left off extension...
 
-[ p,n,~ ] = fileparts(nii_fname);
-nii_fname = fullfile(p,[n '.nii']);
-
+nseeds = size(centers,2);
+    
 if (nargin < 5) 
-    voxel_value = 1;
+    voxel_values = ones(nseeds,1);
+else
+    if (ischar(voxel_value))
+        voxel_values = 1:nseeds;    % "atlas mode"
+    else
+        voxel_values = voxel_value * ones(nseeds,1);
+    end
 end
 
 % if only one radius was passed we apply it to all centers
-
-nseeds = size(centers,2);
 
 if (length(radii) ~= nseeds)
     radii = radii * ones(nseeds,1);
@@ -67,26 +82,36 @@ end
    
 header = spm_vol(template_fname);
 
-seed_XYZ = [];
+seed_XYZ = cell(nseeds,1);
 
 for index = 1:nseeds
     this_center = centers(:,index);
     this_radius = radii(index);
-    [ ~,XYZ ] = extract_voxel_values_around_seed(header, this_center, this_radius);
-    seed_XYZ = [ seed_XYZ XYZ ];        
+    temp = [];
+    [ ~,temp ] = extract_voxel_values_around_seed(header, this_center, this_radius); 
+    seed_XYZ{index} = temp;
 end
 
 % write to nii
+
+% in case caller left off extension...
+
+[ p,n,~ ] = fileparts(nii_fname);
+nii_fname = fullfile(p,[n '.nii']);
 
 fprintf('Creating %s...\n', nii_fname);
 
 Y = spm_read_vols(header);
 Y(:) = 0;
-IJK = header.mat\[ seed_XYZ; ones(1,size(seed_XYZ,2)) ];
-Y(sub2ind(size(Y),IJK(1,:),IJK(2,:),IJK(3,:))) = voxel_value;
+
+for index = 1:nseeds
+    temp = seed_XYZ{index};
+    IJK = header.mat\[ temp; ones(1,size(temp,2)) ];
+    Y(sub2ind(size(Y),IJK(1,:),IJK(2,:),IJK(3,:))) = voxel_values(index);
+end
+
 nifti_write_localcopy(nii_fname, Y, 'ROI2nii', header);
   
-
 end
 
 
